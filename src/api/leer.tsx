a545f7005340086e62ba6818356c68ded9f10d86
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-01-25 16:45:08 -0500

    Dentro de la función useEffect en el componente leer.tsx, se ha implementado el método para cargar archivos utilizando ``fetch´´. Esta carga de archivos depende de una biblioteca llamada json-server para simular un servidor Backend y cargarlos hacia la página.

--- src/api/leer.tsx
+++ src/api/leer.tsx
@@ -1,21 +1,29 @@
-import fs from 'fs/promises';
-import path from 'path';
-import { URL } from '../funciones/URL';
-import res from './resultados.json'
+import { useEffect, useState } from 'react';
 
-export default async function Leer() {  
-    const filePath = path.join(URL(), '../../public', 'resultados.json');
+export default function Leer() {  
     console.log(process.cwd())
-    try {
-        console.log(filePath)
-        const data = await fs.readFile('./resultados.json');
-        //const data = { prueba: 'ddd' };
-        console.log('data')
-        console.log(data);
-        return data;
-    }
-    catch (error) {
-        console.error('No se ha podido cargar el archivo');
-        throw new Error('Archivo no encontrado.');
-    }
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        async function cargar() {
+            try {
+                console.log('Cargando datos')
+                fetch('http://localhost:3001/puestos').then(res => {
+                    console.log('red');
+                    console.log(res);
+                    return res.json();
+                }).then(getData => {
+                    console.log(getData)
+                    setData(getData);
+                });
+                console.log('data')
+                console.log(data);
+            }
+            catch (error) {
+                console.error('No se ha podido cargar el archivo');
+                throw new Error('Archivo no encontrado.');
+            }
+        }
+        cargar();
+    }, [])
+    return data;
 }

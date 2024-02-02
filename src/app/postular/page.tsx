bf997c6ebb6959c553b14ce7140768744df244c2
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-02 17:22:46 -0500

    Implementado los campos de entrada en texto para la página de postular.

---
+++ src/app/postular/page.tsx
@@ -0,0 +1,106 @@
+"use client"
+
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/shadcn-ui/ui/form";
+import {ControllerFieldState, ControllerRenderProps, SubmitHandler, UseFormReturn, UseFormStateReturn, useForm} from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod';
+import * as zod from "zod";
+import { Input } from "@/shadcn-ui/ui/input";
+import { Button } from "@/shadcn-ui/ui/button";
+import { format } from "path";
+
+const phone = zod.string().refine(num => {
+    num.length <= 10;
+    Number(num) === parseInt(num);
+}, {
+    message: "Numero telefónico requiere incluír solo numeros del 0 al 9 y no exceder a más de 10 digitos.",
+});
+
+const zodSend = zod.object({
+    email: zod.string().email(),
+    name: zod.string(),
+    surname: zod.string(),
+    phoneNo: phone,
+});
+
+function enviar(postulacion: zod.infer<typeof zodSend>) {
+    console.log(postulacion);
+}
+
+export default function Postular() {
+    const form = useForm<zod.infer<typeof zodSend>>({
+        resolver: zodResolver(zodSend),
+        defaultValues: {
+            email: "",
+            name: "",
+            surname: "",
+            phoneNo: "",
+        }});
+    return(<div>
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(enviar)}>
+                <FormField control={form.control} name = "email"
+                render={field => {
+                    return (
+                        <FormItem>
+                            <FormLabel>Correo Electrónico</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Email"
+                                    type="email"
+                                    {...field} 
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                        );
+                }}/>
+                <FormField control={form.control} name = "name"
+                    render={(field) => {
+                        return(
+                            <FormItem>
+                                <FormLabel>Nombre</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Nombre"
+                                        type="Nombre"
+                                        {...field}
+                                    />
+                                </FormControl>
+                            </FormItem>
+                        );
+                    }}/>
+                    <FormField control={form.control} name = "surname"
+                    render={(field) => {
+                        return(
+                            <FormItem>
+                                <FormLabel>Apellido(s)</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Apellido(s)"
+                                        type="apellidos"
+                                        {...field}
+                                    />
+                                </FormControl>
+                            </FormItem>
+                        );
+                    }}/>
+                    <FormField control={form.control} name = "phoneNo"
+                    render={(field) => {
+                        return(
+                            <FormItem>
+                                <FormLabel>Numero Telefónico</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Numero Telefónico"
+                                        type="numero"
+                                        {...field}
+                                    />
+                                </FormControl>
+                            </FormItem>
+                        );
+                    }}/>
+                    <Button type="submit">Enviar Postulación</Button>
+            </form>
+        </Form>
+    </div>)
+}

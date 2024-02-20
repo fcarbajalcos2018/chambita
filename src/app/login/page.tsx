83d086c558411d72499762cbc0f781aa2a09980b
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-20 15:30:20 -0500

    Refactorización de código. Implementado la función para el evento login.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,13 +9,16 @@ import { ReactElement, JSXElementConstructor } from "react";
 import { Input } from "@/shadcn-ui/ui/input";
 import { Button } from "@/shadcn-ui/ui/button";
 import { useRouter } from "next/navigation";
+import Leer from "@/api/leer";
 
-const passwordString = zod.string().refine(pw => pw.length >= 8, {
+const Email = zod.string()
+    .email("Correo electrónico requiere ser proporcionado como sigue: nombre@exemplo.com");
+const Password = zod.string().refine(pw => pw.length >= 8, {
     message: "Contraseña debe tener minimo 8 carácteres."
 });
 const zodLogin = zod.object({
-    email: zod.string().email(),
-    password: passwordString
+    email: Email,
+    password: Password
 });
 
 function Login() {
@@ -26,28 +29,58 @@ function Login() {
             password: ""
         }});
         console.log("dddd");
+
+    const cuentas = Leer('http://localhost:3001/cuentas') as any;
+    const route = useRouter();
+
+    function HacerLogin(miCuenta: zod.infer<typeof zodLogin>, webCuenta: any) {
+        console.log("cc");
+        console.log(miCuenta);
+        let credenciales = {
+            email: '',
+            nombre: '',
+            apellido: '',
+            contraseña: '',
+        }
+        
+        if (webCuenta !== null) {
+            credenciales = webCuenta.find((e : any) => e.email === miCuenta.email);
+            console.log(webCuenta);
+        }
+    
+        console.log(credenciales);
+        
+        if (miCuenta.email !== credenciales.email || miCuenta.password !== credenciales.contraseña){
+            console.log(`${miCuenta.email}, ${credenciales.email}, ${miCuenta.password}, ${credenciales.contraseña}`)
+            console.log('eer')
+            return Error('El correo electrónico y/o contraseña no coinciden con una cuenta existente.');
+        }
+        route.push('/');
+    }
+
     return (
         <div>
             <Form {...form}>
-                <form onSubmit={form.handleSubmit(HacerLogin)}>
+                <form onSubmit={form.handleSubmit(
+                    (cred: { email: string; password: string; }) => HacerLogin(cred, cuentas))}>
                     <FormField control={form.control} name="email" 
-                    render={(field) => {
+                    render={({field}) => {
                         return (
-                        <FormItem>
-                            <FormLabel>Correo Electrónico</FormLabel>
-                            <FormControl>
-                                <Input
-                                    placeholder="Email"
-                                    type="email"
-                                    {...field} 
-                                />
-                            </FormControl>
-                            <FormMessage />
-                        </FormItem>
+                            <FormItem>
+                                <FormLabel>Correo Electrónico</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Email"
+                                        type="email"
+                                        {...field} 
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
                         );
                     }}/>
                     <FormField control={form.control} name="password"
-                    render={(field) => {
+                    render={({field}) => {
                         return(
                             <FormItem>
                                 <FormLabel>Contraseña</FormLabel>
@@ -66,12 +99,6 @@ function Login() {
             </Form>
         </div>
     )
-}
-
-function HacerLogin() {
-    console.log("cc");
-    const route = useRouter();
-    route.push('/');
 }
 
 export default Login;

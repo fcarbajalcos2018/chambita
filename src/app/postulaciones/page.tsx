5f2ff2ffc4b680a6628d645c5431f62f1a52178e
fcarbajalcos2018 <fcarbajalcos2018@fau.edu>
2024-02-20 15:34:16 -0500

    Implementado la invocación de la función para leer el archivo JSON del 'servidor'.

--- src/app/postulaciones/page.tsx
+++ src/app/postulaciones/page.tsx
@@ -1,13 +1,44 @@
 "use client"
 
 import { NavBar } from "@/shadcn-ui/navbar";
+import Leer from "@/api/leer";
 
 export default function Postulaciones() {
-    console.log('nav');
+
+    const email = 'frankair@amail.io';
+    const data = Leer('http://localhost:3001/postulaciones');
+    let jsonData = [
+        {
+            "id": "---",
+            "email": "----",
+            "name": "----",
+            "surname": "----",
+            "phoneNo": "-----"
+        }
+    ];
+    if (data != null) {
+        jsonData = data;
+    }
+    console.log(jsonData[0].id);
+    const filtrado = jsonData.filter(elem => elem.email == email);
+    console.log(filtrado);
+
     return (
         <main>
             <div>Mis Postulaciones</div>
             <NavBar/>
+            {
+                filtrado.map(elem => {
+                    return(
+                        <div>
+                            <div>{ elem.id }</div>
+                            <div>{ elem.email }</div>
+                            <div>{ elem.name + ' ' + elem.surname }</div>
+                            <div>{ elem.phoneNo }</div>
+                        </div>
+                    )
+                })
+            }
         </main>
     );
 }
